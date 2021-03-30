020534d3c0921d8f0d75213a0fec471b5a5c4df8
zgl <376281732@qq.com>
2021-03-30 11:33:47 +0800

    update divideSearch.c

--- chapter1/exercise/divideSearch.c
+++ chapter1/exercise/divideSearch.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 
 int divideSearch(int *arr, int left, int right, int want);
-void cover(int *arr, int posX, int posY, int errX, int errY, int *id);
-void chessCover(int *arr, int posX, int posY, int len, int errX, int errY, int *id);
+int seqSearch(int *arr, int len, int want);
 
-int
-main() {
+int main() {
     int arr[10] = {
         1, 7, 78, 91, 92, 99, 101, 198, 234, 235,
     };
@@ -38,77 +36,4 @@ int seqSearch(int *arr, int len, int want) {
         }
     }
     return -1;
-}
-
-// a bad case for long code.
-//void chessCover(int *arr, int posX, int posY, int len, int errX, int errY, int *id) {
-    //if (len == 2) {
-        //for (int i=posX; i<len; i++) {
-            //for (int j=posY; j<len; j++) {
-                //if (i != errX || j != errY) {
-                    //arr[i][j] = *id;
-                //}
-            //}
-        //}
-        //*id += 1;
-        //return;
-    //}
-    //int midX = posX + len/2;
-    //int midY = posY + len/2;
-    //if (errX < midX && errY < midY) {
-        //arr[midX][midY-1] = *id;
-        //arr[midX-1][midY] = *id;
-        //arr[midX][midY] = *id;
-        //*id += 1;
-    //} else if (errX < midX && errY < midY) {
-        ////something
-    //} else if (errX < midX && errY < midY) {
-        ////something
-    //} else {
-        ////something
-    //}
-//}
-
-void chessCover(int *arr, int posX, int posY, int len, int errX, int errY, int *id) {
-    if (len == 2) {
-        cover(arr, posX, posY, errX, errY, id);
-    }
-    int midX = posX + len/2;
-    int midY = posY + len/2;
-    if (errX < midX && errY < midY) {
-        cover(arr, midX-1, midY-1, midX-1, midY-1, id);
-        chessCover(arr, posX, posY, len/2, errX, errY, id);
-        chessCover(arr, posX, midY, len/2, midX-1, midY, id);
-        chessCover(arr, midX, posY, len/2, midX, midY-1, id);
-        chessCover(arr, midX, midY, len/2, midX, midY, id);
-    } else if (errX < midX && errY >= midY) {
-        cover(arr, midX-1, midY-1, midX-1, midY, id);
-        chessCover(arr, posX, posY, len/2, midX-1, midY-1, id);
-        chessCover(arr, posX, midY, len/2, errX, errY, id);
-        chessCover(arr, midX, posY, len/2, midX, midY-1, id);
-        chessCover(arr, midX, midY, len/2, midX, midY, id);
-    } else if (errX >= midX && errY < midY) {
-        cover(arr, midX-1, midY-1, midX, midY-1, id);
-        chessCover(arr, posX, posY, len/2, midX-1, midY-1, id);
-        chessCover(arr, posX, midY, len/2, midX-1, midY, id);
-        chessCover(arr, midX, posY, len/2, errX, errY, id);
-        chessCover(arr, midX, midY, len/2, midX, midY, id);
-    } else {
-        cover(arr, midX-1, midY-1, midX, midY, id);
-        chessCover(arr, posX, posY, len/2, midX-1, midY-1, id);
-        chessCover(arr, posX, midY, len/2, midX-1, midY, id);
-        chessCover(arr, midX, posY, len/2, midX, midY-1, id);
-        chessCover(arr, midX, midY, len/2, errX, errY, id);
-    }
-}
-
-void cover(int *arr, int posX, int posY, int errX, int errY, int *id) {
-    for (int i=posX; i<posX+2; i++) {
-        for (int j=posY; j<posY+2; j++) {
-            if (i != errX || j != errY) {
-                arr[i][j] = *id;
-            }
-        }
-    }
-    *id += 1;
 }
